d8680306a559d0ba99c7d1ad5fecb49b4ffe099f
James Kirk <james.f.kirk@gmail.com>
2022-01-05 11:12:45 -0500

    ENH: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# wordless
+# Wordless
+
+Wordless is the #1 app for helping you cheat at Wordle, which is sure to make you popular at parties.
+
+Run Wordless with the following:
+
+```bash
+pip install -r requirements.txt
+python wordless.py
+```
+
+Wordless will prompt you to enter grey, yellow, and green letters and give you a set of words you can guess to continue.
+
+```
+Welcome to Wordless! The #1 app in the world for helping you cheat at wordle.
+
+So far we know:
+Missing letters:
+[]
+Letters that are present but in the wrong positions:
+[]
+Letters that are present and in the correct positions:
+[]
+
+Current number of candidates: 9972
+Maybe try one of these words next:
+bajri, soapy, bakli, crash, jaunt, chewy, hosta, xinca, adieu, ligne
+
+Please select one of the following:
+1. Input grey letters
+2. Input yellow letters
+3. Input green letters
+4. Quit
+```
---
+++ wordless.py
@@ -0,0 +1,148 @@
+from collections import defaultdict
+import random
+
+import nltk
+from nltk import chunk
+import pyinputplus as pyip
+
+nltk.download("words")
+
+
+def chunkstring(string, length):
+    return (string[0 + i : length + i] for i in range(0, len(string), length))
+
+
+def count_unique_letters_in_string(string):
+    return len(set(string))
+
+
+def select_guesses(candidates):
+    candidates_list = sorted(
+        [
+            (candidate, count_unique_letters_in_string(candidate))
+            for candidate in candidates
+        ],
+        key=lambda x: x[1],
+        reverse=True,
+    )
+
+    num_to_return = min(len(candidates_list), 10)
+    return [candidate for candidate, _ in candidates_list[:num_to_return]]
+
+
+def generate_indices():
+    five_letter_words = {
+        word.lower() for word in nltk.corpus.words.words() if len(word) == 5
+    }
+
+    # The indices go [letter][in/not-in bool][slot]
+    letter_position_indices = defaultdict(lambda: defaultdict(lambda: defaultdict(set)))
+    letter_missing_indices = defaultdict(set)
+
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        for slot in range(0, 5):
+            for word in five_letter_words:
+                if word[slot] == letter:
+                    letter_position_indices[letter][True][slot].add(word)
+                elif letter in word:
+                    letter_position_indices[letter][False][slot].add(word)
+
+        for word in five_letter_words:
+            if word.count(letter) == 0:
+                letter_missing_indices[letter].add(word)
+
+    return five_letter_words, letter_position_indices, letter_missing_indices
+
+
+if __name__ == "__main__":
+    (
+        five_letter_words,
+        letter_position_indices,
+        letter_missing_indices,
+    ) = generate_indices()
+    candidates = five_letter_words.copy()
+
+    all_missing_letters = []
+    all_wrong_pos_letters = []
+    all_correct_pos_letters = []
+
+    print("")
+    print(
+        "Welcome to Wordless! The #1 app in the world for helping you cheat at wordle."
+    )
+
+    running = True
+    while running:
+
+        print("")
+        print("So far we know:")
+        print("Missing letters:")
+        print(all_missing_letters)
+        print("Letters that are present but in the wrong positions:")
+        print(all_wrong_pos_letters)
+        print("Letters that are present and in the correct positions:")
+        print(all_correct_pos_letters)
+
+        print("")
+        print("Current number of candidates: {}".format(len(candidates)))
+        print("Maybe try one of these words next:")
+
+        guesses = select_guesses(candidates)
+        print(", ".join(guesses))
+
+        print("")
+        input_type_choice = pyip.inputMenu(
+            [
+                "Input grey letters",
+                "Input yellow letters",
+                "Input green letters",
+                "Quit",
+            ],
+            numbered=True,
+        )
+
+        # Missing letters entry
+        if input_type_choice == "Input grey letters":
+            print("Enter grey (missing) letters:")
+            print("(e.g. enter 'aghz' to say that a, g, h, z are missing letters)")
+            missing_letters = pyip.inputStr("")
+            # Process missing letters
+            for letter in missing_letters.lower():
+                all_missing_letters.append(letter)
+                candidates = candidates.intersection(letter_missing_indices[letter])
+
+        # Wrong-position letters entry
+        elif input_type_choice == "Input yellow letters":
+            print("Enter yellow (present but wrong position) letters:")
+            print(
+                "(e.g. enter 't1g3' to say that t is not in slot 1 and g is not in slot 3, like in the word 'gamut')"
+            )
+            wrong_position_letters = pyip.inputStr("")
+            pairs = chunkstring(wrong_position_letters, 2)
+            for pair in pairs:
+                all_wrong_pos_letters.append(pair)
+                letter = pair[0]
+                position = int(pair[1])
+                candidates = candidates.intersection(
+                    letter_position_indices[letter][False][position]
+                )
+
+        # Correct letters entry
+        elif input_type_choice == "Input green letters":
+            print("Enter green (correct) letters:")
+            print(
+                "(e.g. enter 't1g3' to say that t is in slot 1 and g is in slot 3, like the word 'tiger')"
+            )
+            correct_letters = pyip.inputStr("")
+            pairs = chunkstring(correct_letters, 2)
+            for pair in pairs:
+                all_correct_pos_letters.append(pair)
+                letter = pair[0]
+                position = int(pair[1]) - 1
+                candidates = candidates.intersection(
+                    letter_position_indices[letter][True][position]
+                )
+
+        # Quit
+        elif input_type_choice == "Quit":
+            running = False
